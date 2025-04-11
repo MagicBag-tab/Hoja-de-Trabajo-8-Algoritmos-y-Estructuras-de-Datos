c2039ee347123ef48c5b0e21a1edd7c807e84b62
MagicBag-tab <est24347@uvg.edu.gt>
2025-04-10 20:28:13 -0600

    Comentarios javadoc en Factory

--- Factory.java
+++ Factory.java
@@ -1,3 +1,32 @@
+import java.util.List;
+
+/*
+ * Fábrica para crear diferentes implementaciones de PriorityQueue.
+ */
 public class Factory {
 
+    /**
+     * Crea una cola de prioridad según la opción seleccionada.
+     */
+    public static PriorityQueue<Paciente> crearPriorityQueue(int opcion, List<Paciente> pacientes) {
+        PriorityQueue<Paciente> cola;
+
+        switch (opcion) {
+            case 1:
+                cola = new VectorHeap<>();
+                for (Paciente p : pacientes) {
+                    if (!cola.offer(p)) {
+                        System.out.println("VectorHeap está lleno, no se pudo agregar: " + p.getName());
+                    }
+                }
+                break;
+            case 2:
+                cola = new PriorityQueueImplementation<>(pacientes);
+                break;
+            default:
+                throw new IllegalArgumentException("Opción no válida");
+        }
+
+        return cola;
+    }
 }
