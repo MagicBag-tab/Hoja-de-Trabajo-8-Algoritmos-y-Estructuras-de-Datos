c7ca6d287042fbb4537222d1130cef5a38141751
MagicBag-tab <est24347@uvg.edu.gt>
2025-04-10 19:38:04 -0600

    Clase renombre la clase de la interfaz para no causar conflictos con la implementacion del vectorheap

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,3 +1,17 @@
-public class PriorityQueue {
+/*
+*Interfaz para las clases que utilizan la cola de prioridad
+*/
 
+import java.util.Iterator;
+
+public interface PriorityQueue<E> {
+    boolean offer(E e);
+
+    E poll();
+
+    E peek();
+
+    int size();
+
+    Iterator<E> iterator();
 }
