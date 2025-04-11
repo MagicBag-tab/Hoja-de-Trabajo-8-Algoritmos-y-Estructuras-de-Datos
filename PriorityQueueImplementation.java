15d438a87f957e9c07a7b3b0730c37fb662f4b37
MagicBag-tab <est24347@uvg.edu.gt>
2025-04-10 20:26:05 -0600

    Se agregaron comentarios javadoc a la clase

--- PriorityQueueImplementation.java
+++ PriorityQueueImplementation.java
@@ -2,10 +2,21 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
+/*
+ * Implementación personalizada de una PriorityQueue usando
+ * java.util.PriorityQueue
+ * con un comparador basado en la prioridad de los pacientes (letras A-E).
+ * Utiliza como ejemplo esta página:
+ * https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html#PriorityQueue(java.util.Collection)
+ */
 public class PriorityQueueImplementation<E extends Paciente> implements PriorityQueue<E> {
 
     private java.util.PriorityQueue<E> queue;
 
+    /*
+     * Constructor que crea una cola de prioridad vacía usando un comparador
+     * personalizado que ordena a los pacientes por su nivel de prioridad
+     */
     public PriorityQueueImplementation() {
         this.queue = new java.util.PriorityQueue<>(new Comparator<E>() {
             @Override
@@ -15,16 +26,25 @@ public class PriorityQueueImplementation<E extends Paciente> implements Priority
         });
     }
 
+    /*
+     * Constructor que inicializa la cola con una lista de pacientes.
+     */
     public PriorityQueueImplementation(List<? extends E> pacientes) {
         this();
         queue.addAll(pacientes);
     }
 
+    /*
+     * Inserta un paciente en la cola.
+     */
     @Override
     public boolean offer(E e) {
         return queue.offer(e);
     }
 
+    /*
+     * Elimina y retorna el siguiente paciente con mayor prioridad.
+     */
     @Override
     public E poll() {
         E paciente = queue.poll();
@@ -35,21 +55,33 @@ public class PriorityQueueImplementation<E extends Paciente> implements Priority
         return paciente;
     }
 
+    /*
+     * Retorna el siguiente paciente sin eliminarlo.
+     */
     @Override
     public E peek() {
         return queue.peek();
     }
 
+    /*
+     * Retorna el número de pacientes en la cola.
+     */
     @Override
     public int size() {
         return queue.size();
     }
 
+    /*
+     * Devuelve un iterador para recorrer los pacientes de la cola.
+     */
     @Override
     public Iterator<E> iterator() {
         return queue.iterator();
     }
 
+    /*
+     * Método para atender y mostrar al siguiente paciente.
+     */
     public void atenderSiguientePaciente() {
         poll();
     }
